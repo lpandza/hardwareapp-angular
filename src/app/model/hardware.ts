58e0885ad173a7d6dc86f839177c4a2260c6084e
Luka Pandza <lukapandza@hotmail.com>
2022-05-12 14:22:58 +0200

    lab6

--- src/app/model/hardware.ts
+++ src/app/model/hardware.ts
@@ -1,7 +1,10 @@
+import {Review} from "./review";
+
 export interface Hardware {
   code: string,
   name: string,
   type: string,
   quantity: number,
-  price: number
+  price: number,
+  reviews: Review[]
 }
